650b214a8207d736df57849a71eb5931f49f970f
Charles F. Bearden <Charles.F.Bearden@uth.tmc.edu>
2012-03-16 15:54:45 -0500

    Restructure tests for new architecture; properties covered.

--- tests/test_medication.py
+++ tests/test_medication.py
@@ -209,142 +209,200 @@ class TestParsedMedicationClass(unittest.TestCase):
     
     def setUp(self):
         self.original     = 'Protonix 40 MG Tablet Delayed Release;TAKE 1 TABLET DAILY.; Rx'
+        self.normalized   = 'PROTONIX 40 MG TABLET DELAYED RELEASE;TAKE 1 TABLET DAILY.; RX'
         self.original_generic = 'Sertraline 50 MG Tablet;TAKE 1 TABLET DAILY.; RPT'
         self.name         = 'Protonix'
         self.normalized_name = 'PROTONIX'
-        self.name_new     = 'Mirapex;'
-        self.normalized_name_new = 'MIRAPEX'
+        # self.name_new     = 'Mirapex;'
+        # self.normalized_name_new = 'MIRAPEX'
         self.dose         = '40'
-        self.dose_new     = '30'
+        self.normalized_dose = '40'
+        # self.dose_new     = '30'
         self.units        = 'MG'
-        self.units_new    = 'cl'
-        self.normalized_units_new    = 'CL'
+        self.normalized_units = 'MG'
+        # self.units_new    = 'cl'
+        # self.normalized_units_new    = 'CL'
         self.formulation  = 'Tablet Delayed Release'
         self.normalized_formulation  = 'TABLET DELAYED RELEASE'
-        self.formulation_new = 'Elixir'
-        self.normalized_formulation_new = 'ELIXIR'
+        # self.formulation_new = 'Elixir'
+        # self.normalized_formulation_new = 'ELIXIR'
         self.instructions = 'TAKE 1 TABLET DAILY.; Rx'
         self.normalized_instructions = 'TAKE 1 TABLET DAILY.; RX'
-        self.instructions_new = 'Take 1 Tablet 3 Times Daily.'
-        self.normalized_instructions_new = 'TAKE 1 TABLET 3 TIMES DAILY'
+        self.normalized_dose = '40 MG*1*1'
+        self.provenance   = 'List 42'
+        # self.instructions_new = 'Take 1 Tablet 3 Times Daily.'
+        # self.normalized_instructions_new = 'TAKE 1 TABLET 3 TIMES DAILY'
         self.generic_formula = ['PANTOPRAZOLE (AS PANTOPRAZOLE SODIUM SESQUIHYDRATE)', 'PANTOPRAZOLE SODIUM']
         self.generic_formula.sort()
-        self.mapped_cuis  = set(['C0876139'])
-        self.provenance   = 'List 42'
-        self.normalized_dose = '40 MG*1*1'
+        self.CUIs         = set(['C0876139'])
         self.original_dict = {
-          'medicationName' : self.normalized_name,
-          'dose'           : self.dose,
-          'units'          : self.units,
-          'formulation'    : self.normalized_formulation,
-          'instructions'   : self.normalized_instructions,
-          'original_string': self.original,
-          'provenance'     : self.provenance,
-          'normalized_dose': self.normalized_dose,
-          'parsed'         : True,
+          'medication_name' : self.normalized_name,
+          'dose'            : self.dose,
+          'units'           : self.units,
+          'formulation'     : self.normalized_formulation,
+          'instructions'    : self.normalized_instructions,
+          'original_string' : self.original,
+          'provenance'      : self.provenance,
+          'normalized_dose' : self.normalized_dose,
         }
         self.constructed  = medication.ParsedMedication(self.original)
         self.constructed_mappings  = medication.ParsedMedication(self.original, mappings, self.provenance)
-        self.init_from_text    = medication.ParsedMedication()
-        self.init_from_text.from_text(self.original)
-        self.init_no_text = medication.ParsedMedication()
+        #self.init_from_text    = medication.ParsedMedication()
+        #self.init_from_text.from_text(self.original)
+        #self.init_no_text = medication.ParsedMedication()
 
     def test_construct_no_text(self):
-        self.assertTrue(medication.ParsedMedication(), "Unable to construct ParsedMedication without text.")
+        """Must pass a string to the constructor."""
+        self.assertRaises(TypeError, medication.ParsedMedication)
     
-    def test_constructed(self):
-        self.assertEqual(self.constructed.original_string, self.original,"ParsedMedication class wasn't properly initialized from constructor.")
+    def test_c_original_string_equals(self):
+        self.assertEqual(self.constructed.original_string, self.original,
+          "ParsedMedication class wasn't properly initialized from constructor.")
 
-    def test_init_from_text(self):
-        self.assertEqual(self.init_from_text.original_string, self.original, "ParsedMedication class wasn't properly initialized with .from_text().")
+    def test_cm_original_string_equals(self):
+        self.assertEqual(self.constructed_mappings.original_string, self.original,
+          "ParsedMedication class wasn't properly initialized from constructor.")
+
+    def test_c_original_string_readonly(self):
+        self.assertRaises(TypeError, self.constructed.__setattr__, 'original_string', 'foo') 
+
+    def test_c_normalized_string_equals(self):
+        self.assertEqual(self.constructed.normalized_string, self.normalized,
+          "ParsedMedication class wasn't properly initialized from constructor.")
+
+    def test_cm_normalized_string_equals(self):
+        self.assertEqual(self.constructed_mappings.normalized_string, self.normalized,
+          "ParsedMedication class wasn't properly initialized from constructor.")
+
+    def test_c_normalized_string_readonly(self):
+        self.assertRaises(TypeError, self.constructed.__setattr__, 'normalized_string', 'foo')
+
+#    def test_init_from_text(self):        
+#        self.assertEqual(self.init_from_text.original_string, self.original, "ParsedMedication class wasn't properly initialized with .from_text().")
         
     def test_name_get(self):
         self.assertEqual(self.constructed.name, self.normalized_name)
 
-    def test_name_set(self):
-        self.constructed.name = self.name_new
-        self.assertEqual(self.constructed.name, self.normalized_name_new)
+    def test_name_readonly(self):
+        self.assertRaises(AttributeError, self.instance.__setattr__,
+          'name', "Dr. Jimson's Pantonic Remedy")
     
     def test_dose_get(self):
-        self.assertEqual(self.constructed.dose, self.dose)
+        self.assertEqual(self.constructed.dose, self.normalized_dose)
 
-    def test_dose_set(self):
-        self.constructed.dose = self.dose_new
-        self.assertEqual(self.constructed.dose, self.dose_new)
+    def test_dose_readonly(self):
+        self.assertRaises(AttributeError, self.instance.__setattr__,
+          'dose', '1')
 
     def test_units_get(self):
-        self.assertEqual(self.constructed.units, self.units)
+        self.assertEqual(self.constructed.units, self.normalized_units)
 
-    def test_units_set(self):
-        self.constructed.units = self.units_new
-        self.assertEqual(self.constructed.units, self.normalized_units_new)
+    def test_units_readonly(self):
+        self.assertRaises(AttributeError, self.instance.__setattr__,
+          'units', 'bottle')
 
     def test_formulation_get(self):
         self.assertEqual(self.constructed.formulation, self.normalized_formulation)
 
-    def test_formulation_set(self):
-        self.constructed.formulation = self.formulation_new
-        self.assertEqual(self.constructed.formulation, self.normalized_formulation_new)
+    def test_formulation_readonly(self):
+        self.assertRaises(AttributeError, self.instance.__setattr__,
+          'formulation', 'elixir')
 
     def test_instructions_get(self):
         self.assertEqual(self.constructed.instructions, self.normalized_instructions)
 
-    def test_instructions_set(self):
-        self.constructed.instructions = self.instructions_new
-        self.assertEqual(self.constructed.instructions, self.normalized_instructions_new)
+    def test_instructions_readonly(self):
+        self.assertRaises(AttributeError, self.instance.__setattr__,
+          'instructions', 'Drink 1 tblsp every 4 hours')
 
-    def test_original_line(self):
-        self.assertEqual(self.init_from_text.original_line, self.original)
+    def test_normalized_dose_get(self):
+        self.assertEqual(self.constructed.normalized_dose, self.normalized_dose)
 
-    def test_parsed_constructed(self):
-        self.assertTrue(self.constructed.parsed)
+    def test_normalized_dose_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'normalized_dose', '1 Tblsp*6*1')
+
+    def test_provenance_get(self):
+        self.assertEqual(self.constructed.provenance, self.provenance)
+
+    def test_provenance_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'provenance', 'Lot 49')
+
+    def test_dictionary_get(self):
+        test_dict_set = set(test.original_dict.items())
+        obj_dict_set  = set(self.constructed.dictionary.items())
+        self.assertTrue(test_dict_set <= obj_dict_set)
+
+    def test_dictionary_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'dictionary', {'foo':'bar'})
+
+#    def test_original_line(self):
+#        self.assertEqual(self.init_from_text.original_line, self.original)
+
+#    def test_parsed_constructed(self):
+#        self.assertTrue(self.constructed.parsed)
         
-    def test_parsed_from_text(self):
-        self.assertTrue(self.init_from_text.parsed)
+#    def test_parsed_from_text(self):
+#        self.assertTrue(self.init_from_text.parsed)
         
-    def test_parsed_no_text(self):
-        self.assertTrue(not self.init_no_text.parsed)
+#    def test_parsed_no_text(self):
+#        self.assertTrue(not self.init_no_text.parsed)
         
-    def test_generic_formula_none(self):
-        self.assertRaises(medication.MappingContextError, self.constructed.CUIs, None)
-
-    def test_generic_formula_setter(self):
-        self.constructed.mappings = mappings
-        generic_formula = self.constructed.generic_formula
-        generic_formula.sort()
-        self.assertEquals(self.generic_formula, generic_formula)
-
-    def test_generic_formula_constructed(self):
+    def test_generic_formula_get(self):
         generic_formula = self.constructed_mappings.generic_formula
         generic_formula.sort()
-        self.assertEquals(self.generic_formula, generic_formula)
+        self.assertEqual(generic_formula, self.generic_formula)
 
-    def test_compute_generics_nomapping(self):
-        self.assertRaises(medication.MappingContextError, self.constructed.__getattribute__, 'generic_formula')
+    def test_generic_formula_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'generic_formula', 'Generic Elixir')
 
-    def test_compute_generics(self):
-        self.constructed.compute_generics(mappings)
-        generic_formula = self.constructed_mappings.generic_formula
-        generic_formula.sort()
-        self.assertEquals(self.generic_formula, generic_formula)
+    def test_generic_formula_nomappings(self):
+        self.assertRaises(medication.MappingContextError, self.context.__getattr__, 'generic_formula')
+
+#    def test_compute_generics_nomapping(self):
+#        self.assertRaises(medication.MappingContextError, self.constructed.__getattribute__,
+#          'generic_formula')
+#
+#    def test_compute_generics(self):
+#        self.constructed.compute_generics(mappings)
+#        generic_formula = self.constructed_mappings.generic_formula
+#        generic_formula.sort()
+#        self.assertEquals(self.generic_formula, generic_formula)
         
-    def test_CUIs_none(self):
-        self.assertRaises(medication.MappingContextError, self.constructed.CUIs, None)
+    def test_CUIs_get(self):
+        CUIs = list(self.constructed_mappings.CUIs)
+        CUIs.sort()
+        self.assertEqual(CUIs, self.CUIs)
 
-    def test_CUIs_mapping_arg(self):
-        self.assertEqual(self.constructed.CUIs(mappings), self.mapped_cuis)
+    def test_CUIs_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'CUIs', set(['C02','C01']))
 
-    def test_CUIs_mapping_set(self):
-        self.constructed.mappings = mappings
-        self.assertEqual(self.constructed.CUIs(), self.mapped_cuis)
+    def test_CUIs_nomappings(self):
+        self.assertRaises(medication.MappingContextError,
+          self.context.__getattr__, 'CUIs')
 
-#    def test_tradenames(self): pass
-#    def test_normalize_dose(self): pass
-#    def test_normalized_dose(self): pass
-#    def test_as_dictionary(self):
-#        original_dict = self.constructed_mappings.as_dictionary()
-#        for key, val in self.original_dict.items():
-#            self.assertEquals(original_dict[key], val)
+    def test_tradenames_get(self):
+        tradenames = list(self.constructed_mappings.tradenames)
+        tradenames.sort()
+        self.assertEqual(tradenames, self.tradenames)
+
+    def test_tradenames_readonly(self):
+        self.assertRaises(TypeError, self.constructed_mappings.__setattr__,
+          'tradenames', set(['C02','C01']))
+
+    def test_tradenames_nomappings(self):
+        self.assertRaises(medication.MappingContextError,
+          self.context.__getattr__, 'tradenames')
+
+#    def test__normalize_drug_name(self): pass
+#    def test__compute_generics(self): pass
+#    def test__compute_cuis(self): pass
+#    def test__compute_tradenames(self): pass
+#    def test__normalize_dose(self): pass
 #    def test_fieldwise_comparison(self): pass
 
 
