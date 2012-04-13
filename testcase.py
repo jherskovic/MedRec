5d9ae0732838dd4bae967a67d897dc2ec7a51db5
Charles F. Bearden <Charles.F.Bearden@uth.tmc.edu>
2012-04-13 10:11:46 -0500

    Blah blah blah

--- testcase.py
+++ testcase.py
@@ -6,6 +6,9 @@ from constants import demo_list_2
 import cPickle as pickle
 import bz2
 import re
+import logging
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 id_regex = re.compile(r'(?:0x[0-9a-f]{7,})')
 
@@ -20,8 +23,8 @@ def rmMedIds(repr_string):
 def rmAllIds(repr_string):
     return(rmMedIds(rmObjIds(repr_string)))
 
-rx = pickle.load(bz2.BZ2File('../MedRec/tests/rxnorm.pickle.bz2', 'r'))
-#rx = pickle.load(bz2.BZ2File('../MedRec/rxnorm.pickle.bz2', 'r'))
+#rx = pickle.load(bz2.BZ2File('../MedRec/tests/rxnorm.pickle.bz2', 'r'))
+rx = pickle.load(bz2.BZ2File('../MedRec/rxnorm.pickle.bz2', 'r'))
 ts = pickle.load(bz2.BZ2File('../MedRec/treats.pickle.bz2', 'r'))
 mappings = MappingContext(rx, ts)
 
@@ -37,42 +40,9 @@ meds_list_2 = [pm for pm in
 #meds_list_1 = [ParsedMedication(med_1, mappings, "List 1")]
 #meds_list_2 = [ParsedMedication(med_2, mappings, "List 2")]
 
-matched_by_brand_name = match.match_by_brand_name(meds_list_1, meds_list_2)
-matched_by_brand_name_rev = match.match_by_brand_name(meds_list_2, meds_list_1)
-
-mfor = matched_by_brand_name
-mrev = matched_by_brand_name_rev
-
-#import pdb
-#pdb.set_trace()
-
-print "List1 forward"
-mfor_list1 = [repr(x) for x in mfor.list1]
-mfor_list1.sort()
-print '\n'.join(mfor_list1)
-
-print "List1 reversed"
-mrev_list1 = [repr(x) for x in mrev.list1]
-mrev_list1.sort()
-print '\n'.join(mrev_list1)
-
-print "List2 forward"
-mfor_list2 = [repr(x) for x in mfor.list2]
-mfor_list2.sort()
-print '\n'.join(mfor_list2)
-
-print "List2 reversed"
-mrev_list2 = [repr(x) for x in mrev.list2]
-mrev_list2.sort()
-print '\n'.join(mrev_list2)
-
-print "Reonciled forward"
-mfor_reconciled = [repr(x) for x in mfor.reconciled]
-mfor_reconciled.sort()
-print '\n'.join(mfor_reconciled)
-
-print "Reonciled reversed"
-mrev_reconciled = [repr(x) for x in mrev.reconciled]
-mrev_reconciled.sort()
-print '\n'.join(mrev_reconciled)
-
+# <Potential reconciliation (66.67% certainty; Ingredient lists match) 
+# <Medication 3 @ 0x3626d10: 'PROTONIX' 40 'MG' 'TABLET DELAYED RELEASE' ('TAKE 1 TABLET DAILY.; RX')> <-> 
+# <Medication 15 @ 0x37f4210: 'PANTOPRAZOLE SODIUM' 40 'MG' 'TABLET DELAYED RELEASE' ('TAKE 1 TABLET DAILY.; RX')> @ 0x37f4510>
+med1 = ParsedMedication('Protonix 40 MG Tablet Delayed Release;TAKE 1 TABLET DAILY.; Rx', mappings, 'List 1')
+med2 = ParsedMedication('Pantoprazole Sodium 40 MG Tablet Delayed Release;TAKE 1 TABLET DAILY.; Rx', mappings, 'List 2')
+mbi = match.match_by_ingredients([med1], [med2], min_match_threshold=0.7)
