7b3e2e7bc1765fc32b6b987e78039e021a1fb756
Charles Bearden <Charles.F.Bearden@uth.tmc.edu>
2012-04-17 14:19:51 -0500

    Go on from here.

--- testcase.py
+++ testcase.py
@@ -7,6 +7,7 @@ import cPickle as pickle
 import bz2
 import re
 import logging
+import sys
 
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
@@ -19,6 +20,7 @@ Mirapex 0.5 MG Tablet;TAKE 1 TABLET 3 TIMES DAILY.; Rx
 Lisinopril 5 MG Tablet;TAKE  TABLET TWICE DAILY; Rx
 Coreg 25 MG Tablet;TAKE 1 TABLET TWICE DAILY,  WITH MORNING AND EVENING MEAL; RPT
 Enalapril 20mg;TAKE 1 TABLET TWICE DAILY,  WITH MORNING AND EVENING MEAL; RPT
+Naproxen 250 MG Tablet; TAKE 1 TABLET EVERY 12 HOURS AS NEEDED.
 """.split('\n')
 demo_list_2 = """Warfarin Sodium 2.5 MG Tablet;TAKE AS DIRECTED.; Rx
 Aspirin 325 mg;TAKE 1 TABLET DAILY AS DIRECTED.; Rx
@@ -28,16 +30,39 @@ Norvasc 5 mg;TAKE 1 TABLET TWICE DAILY; Rx
 Kayexalate 15 g;TAKE 1 TABLET DAILY.; Rx
 Toprol-XL 50;TAKE 1 TABLET DAILY.; Rx
 enalapril 20 mg;TAKE 1 TABLET DAILY.; Rx
-Vytorin 10/40;TAKE 1 TABLET 3 TIMES DAILY.; Rx.""".split('\n')
+Vytorin 10/40;TAKE 1 TABLET 3 TIMES DAILY.; Rx.
+Celebrex 200 MG Capsule; TAKE 1 CAPSULE TWICE DAILY WITH FOOD.""".split('\n')
 
-new_list_1, new_list_2, toss = open('docpairs/pair0000005.txt').read().split('******')
-demo_list_1 = new_list_1.split('\n')
-demo_list_2 = new_list_2.split('\n')
+#new_list_1, new_list_2, toss = open('docpairs/pair0000005.txt').read().split('******')
+#demo_list_1 = new_list_1.split('\n')
+#demo_list_2 = new_list_2.split('\n')
 
-rx = pickle.load(bz2.BZ2File('../MedRec/tests/rxnorm.pickle.bz2', 'r'))
-#rx = pickle.load(bz2.BZ2File('../MedRec/rxnorm.pickle.bz2', 'r'))
+#rx = pickle.load(bz2.BZ2File('../MedRec/tests/rxnorm.pickle.bz2', 'r'))
+rx = pickle.load(bz2.BZ2File('../MedRec/rxnorm.pickle.bz2', 'r'))
 ts = pickle.load(bz2.BZ2File('../MedRec/treats.pickle.bz2', 'r'))
 mappings = MappingContext(rx, ts)
+
+#Paxil = ts['C0376414']
+#Paroxetine = ts['C0070122']
+#Zoloft = ts['C0284660']
+#Sertraline = ts['C0074393']
+#duloxetine = ts['C0245561']
+#Cymbalta = ts['C1505021']
+#
+#print Paxil & Paroxetine & Zoloft & Sertraline & duloxetine & Cymbalta
+
+#Acetominophen = ts['C0000970']
+#Ibuprofen = ts['C0020740']
+#Naproxen = ts['C0027396']
+#Celecoxib = ts['C0538927']
+##Advil = ts['C0593507']
+##Tylenol = ts['C0699142']
+##Motrin = ts['C0699203']
+##Aleve = ts['C0718343']
+#Celebrex = ts['C0719198']
+#
+#print Acetominophen & Ibuprofen & Naproxen & Celecoxib & Celebrex
+#sys.exit()
 
 #meds_list_1 = [pm for pm in
 #  [ParsedMedication(x, mappings, "List 1") for x in demo_list_1]
@@ -46,11 +71,24 @@ mappings = MappingContext(rx, ts)
 #  [ParsedMedication(x, mappings, "List 2") for x in demo_list_2]
 #    if pm.parsed]
 
-meds_list_1 = [ParsedMedication(x, mappings, "List 1") for x in demo_list_1]
-meds_list_2 = [ParsedMedication(x, mappings, "List 2") for x in demo_list_2]
+'Naproxen 250 MG Tablet; TAKE 1 TABLET EVERY 12 HOURS AS NEEDED.'
+'Tylenol 325 MG Oral Tablet; TAKE 1 TO 2 TABLETS EVERY 6 HOURS AS NEEDED.'
+'Celebrex 200 MG Capsule; TAKE 1 CAPSULE TWICE DAILY WITH FOOD.'
+'Celecoxib 200 MG Capsule; TAKE 1 CAPSULE TWICE DAILY WITH FOOD.'
+
+meds_list_1 = [pm for pm in [ParsedMedication(x, mappings, "List 1") for x in demo_list_1] if pm.parsed]
+meds_list_2 = [pm for pm in [ParsedMedication(x, mappings, "List 2") for x in demo_list_2] if pm.parsed]
+
+meds_list_1 = [meds_list_1[-1]]
+meds_list_2 = [meds_list_2[-1]]
+
+ml1 = [ParsedMedication('Aleve 200 MG Capsule; TAKE 1 CAPSULE TWICE DAILY WITH FOOD.', mappings)]
+ml2 = [ParsedMedication('Naproxen 200 MG Capsule; TAKE 1 CAPSULE TWICE DAILY WITH FOOD.', mappings)]
 
 #matchResult = match.match_by_treatment(meds_list_1, meds_list_2, mappings)
-mbs = match.match_by_strings(meds_list_1, meds_list_2)
-mbbn = match.match_by_brand_name(meds_list_1, meds_list_2)
+#mbs = match.match_by_strings(meds_list_1, meds_list_2)
+#mbbn = match.match_by_brand_name(meds_list_1, meds_list_2)
+#mbi = match.match_by_ingredients(meds_list_1, meds_list_2)
+#mbt = match.match_by_treatment(meds_list_1, meds_list_2, mappings)
 mbi = match.match_by_ingredients(meds_list_1, meds_list_2)
-mbt = match.match_by_treatment(meds_list_1, meds_list_2, mappings)
+mbt = match.match_by_treatment(ml1, ml2, mappings)
