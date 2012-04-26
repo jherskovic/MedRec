5c973f9ec9b7a58cf7b1b9b69c7ef83595ad070a
Charles F. Bearden <Charles.F.Bearden@uth.tmc.edu>
2012-04-26 10:17:42 -0500

    Build StringIO files just before each usage & get rid of all those .seek(0) calls; when testing constructors, check not just that the object is True but that it is an instance of the expected type; add lots of docstrings.

--- tests/test_rxnorm.py
+++ tests/test_rxnorm.py
@@ -25,7 +25,6 @@ C0000003|T131|A1.4.1.1.5|Hazardous or Poisonous Substance|AT123456789|256|
 C0000004|T109|A1.4.1.2.1|Organic Chemical|AT123456789|256|
 C0000004|T121|A1.4.1.1.1|Pharmacologic Substance|AT123456789|256|
 """
-semtypesFile = StringIO(semtypesData)
 
 class TestSemanticTypeLine(unittest.TestCase):
 
@@ -51,19 +50,19 @@ class TestSemanticTypeLine(unittest.TestCase):
       'Hazardous or Poisonous Substance' : ['C0000003',],
     }
     semtypeObjects = []
-    semtypesFile.seek(0)
+    semtypesFile = StringIO(semtypesData)
     for semtypeLine in semtypesFile:
         stl = rxnorm.SemanticTypeLine(semtypeLine)
         semtypeObjects.append(stl)
     
-    def setUp(self):
-        pass
-    
     def test_constructor(self):
+        """Test that all SemanticTypeLine objects got built."""
         for semtypeObject in self.semtypeObjects:
-            self.assertTrue(semtypeObject)
+            self.assertTrue(isinstance(semtypeObject, rxnorm.SemanticTypeLine))
 
     def test_semtype_get(self):
+        """Test that SemanticTypeLine.semType property yields the expected 
+        values from our test objects."""
         i = 0
         while i < len(self.semtypeObjects):
             self.assertEqual(self.semtypeObjects[i].semtype,
@@ -71,10 +70,13 @@ class TestSemanticTypeLine(unittest.TestCase):
             i += 1
 
     def test_semtype_readonly(self):
+        """Test that the .semType property is immutable."""
         stl = random.sample(self.semtypeObjects, 1)[0]
         self.assertRaises(AttributeError, stl.__setattr__, 'semtype', 'Foo')
 
     def test_CUI_get(self):
+        """Test that .CUI property yields the expected values 
+        from our test objects."""
         i = 0
         while i < len(self.semtypeObjects):
             self.assertEqual(self.semtypeObjects[i].CUI,
@@ -82,6 +84,7 @@ class TestSemanticTypeLine(unittest.TestCase):
             i += 1
 
     def test_CUI_readonly(self):
+        """Test that the .CUI property is immutable."""
         stl = random.sample(self.semtypeObjects, 1)[0]
         self.assertRaises(AttributeError, stl.__setattr__, 'CUI', 'Foo')
 
@@ -91,7 +94,6 @@ C0000001|ENG|P|L0000001|PF|S0000001|N|A00000001|123345|7685||RXNORM|BN|768|Synfl
 C0000003|ENG|P|L0000003|PF|S0000003|N|A00000003|360443|1298||RXNORM|IN|11289|Warfarin|0|N|256|
 C0000004|ENG|P|L0000004|PF|S0000004|N|A00000004|395370|2302||RXNORM|IN|20352|carvedilol|0|N|256|
 """
-drugsFile = StringIO(drugsData)
 
 class TestDrug(unittest.TestCase):
     
@@ -108,12 +110,12 @@ class TestDrug(unittest.TestCase):
     brandNamesByLine = (False, True, False, False)
 
     def setUp(self):
-        self.drugObjects = self.make_drugObjects(drugsFile)
+        self.drugObjects = self.make_drugObjects(StringIO(drugsData))
 
     def test_constructor(self):
         """Test that all drug objects got built."""
         for drugObject in self.drugObjects:
-            self.assertTrue(drugObject)
+            self.assertTrue(isinstance(drugObject, rxnorm.Drug))
 
     def test_cui_get(self):
         """Test that CUIs are correctly read from their Drug objects."""
@@ -129,8 +131,9 @@ class TestDrug(unittest.TestCase):
 
     @classmethod
     def make_drugObjects(self, drugsFile):
+        """Helper function that initializes a Drug object from each line in 
+        the file object drugsFile."""
         drugObjects = []
-        drugsFile.seek(0)
         for drugLine in drugsFile:
             if drugLine.strip() == '':
                 continue
@@ -143,7 +146,6 @@ class TestDrug(unittest.TestCase):
         """Helper method to make a dictionary of semantic types:
             CUI -> [<semantic type name>, ...]"""
         semtypesDict = {}
-        semtypesFile.seek(0)
         for semtypeLine in semtypesFile:
             stl = rxnorm.SemanticTypeLine(semtypeLine)
             cui = stl.CUI
@@ -166,7 +168,7 @@ class TestDrug(unittest.TestCase):
 
     def test_semtypes_get(self):
         """Test that semantic types are correctly read from their Drug objects."""
-        self.semtypes_set(self.drugObjects, semtypesFile)
+        self.semtypes_set(self.drugObjects, StringIO(semtypesData))
         i = 0
         while i < len(self.drugObjects):
             myDrug = self.drugObjects[i]
@@ -203,7 +205,6 @@ C0000001|A0000001|SCUI|RO|C0000010|A0000010|SCUI|has_ingredient|R45440713||RXNOR
 C0000008|A0000008|SCUI|RO|C0000003|A0000003|SCUI|ingredient_of|R111424489||RXNORM|RXNORM|||N||
 C0000007|A0000007|SCUI|RB|C0000004|A0000004|SCUI|has_form|R57803279||RXNORM|RXNORM|||N||
 """
-relsFile = StringIO(relsData)
 
 class TestRelation(unittest.TestCase):
 
@@ -215,7 +216,6 @@ C0000008|ENG|P|L1895717|PF|S2895019|Y|A16944408|2978151|855295||RXNORM|SCDC|8552
 C0000009|ENG|P|L9472585|PF|S11770019|Y|A18445510|3297943|1008561||RXNORM|MIN|1008561|Acetaminophen / Naproxen|0|N||
 C0000010|ENG|P|L5577617|PF|S6390231|Y|A10664536|2055106|379829||RXNORM|SBDF|379829|Naproxen Oral Tablet [Synflex]|0|O||
 """
-    drugsPenumbraFile = StringIO(drugsPenumbraData)
 
     semtypesPenumbra = """C0000005|T109|A1.4.1.2.1|Organic Chemical|AT08642239|256|
 C0000005|T121|A1.4.1.1.1|Pharmacologic Substance|AT07968750|256|
@@ -237,30 +237,32 @@ C0000010|T200|A1.3.3|Clinical Drug|AT37405600||
       ('C0000001', 'C0000010', 'has_ingredient',),
       ('C0000008', 'C0000003', 'ingredient_of',),
       ('C0000007', 'C0000004', 'has_form',)])
-    semtypesFile = StringIO(semtypesData + semtypesPenumbra)
 
     def setUp(self):
-        drugObjects = TestDrug.make_drugObjects(drugsFile) + TestDrug.make_drugObjects(self.drugsPenumbraFile)
-        TestDrug.semtypes_set(drugObjects, self.semtypesFile)
+        drugObjects = TestDrug.make_drugObjects(StringIO(drugsData)) + TestDrug.make_drugObjects(StringIO(self.drugsPenumbraData))
+        TestDrug.semtypes_set(drugObjects, StringIO(semtypesData + self.semtypesPenumbra))
         concepts = {}
         for drugObject in drugObjects:
             concepts[drugObject.CUI] = drugObject
-        self.relObjects = self.make_relObjects(relsFile, concepts)
+        self.relObjects = self.make_relObjects(StringIO(relsData), concepts)
 
     @classmethod
     def make_relObjects(self, relsFile, concepts):
+        """Helper function to build a list of Relation objects from the
+        given file and dictionary of CUI -> Drug mappings."""
         relObjects = []
-        relsFile.seek(0)
         for relLine in relsFile:
             r = rxnorm.Relation(relLine, concepts)
             relObjects.append(r)
         return relObjects
     
     def test_relObjects(self):
+        """Test that all Relation objects got built."""
         for relObject in self.relObjects:
-            self.assertTrue(relObject)
+            self.assertTrue(isinstance(relObject, rxnorm.Relation))
 
     def test_relations(self):
+        """Test that the Relation objects contain the expected relations."""
         actualRels = set()
         for relObject in self.relObjects:
             relTuple = (relObject.concept1.CUI, relObject.concept2.CUI, relObject.relation)
@@ -268,18 +270,21 @@ C0000010|T200|A1.3.3|Clinical Drug|AT37405600||
         self.assertTrue(actualRels == self.testRels)
     
     def test_concept1_readonly(self):
+        """Test that the .concept1 property is immutable."""
         relObj = random.sample(self.relObjects, 1)
-        drugsFile.seek(0)
+        drugsFile = StringIO(drugsData)
         drugObj = rxnorm.Drug(drugsFile.readline())
         self.assertRaises(AttributeError, relObj.__setattr__, 'concept1', drugObj)
 
     def test_concept2_readonly(self):
+        """Test that the .concept2 property is immutable."""
         relObj = random.sample(self.relObjects, 1)
-        drugsFile.seek(0)
+        drugsFile = StringIO(drugsData)
         drugObj = rxnorm.Drug(drugsFile.readline())
         self.assertRaises(AttributeError, relObj.__setattr__, 'concept2', drugObj)
 
     def test_relation_readonly(self):
+        """Test that the .relation property is immutable."""
         relObj = random.sample(self.relObjects, 1)
         self.assertRaises(AttributeError, relObj.__setattr__, 'relation', 'is_caffeinated')
 
